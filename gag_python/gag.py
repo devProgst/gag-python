0526f428e61be8b70cbc634493321d6e0a62554f
IvanovAA <dev.progst@gmail.com>
2022-06-28 13:06:57 +0300

    Changes

--- gag_python/gag.py
+++ gag_python/gag.py
@@ -1,17 +1,16 @@
-from doctest import testfile
 import time
-import os
-import sys
 import cv2 as cv
 import hydra
-from omegaconf import DictConfig
 
 class GAGModel():
     """
     Main GAG class of predict age and gender python script.
     """
 
-    def __init__(self, cfg: DictConfig):
+    def __init__(self):
+        hydra.initialize(config_path="conf", job_name="test_app")
+        cfg = hydra.compose(config_name="config")
+
         faceProto = cfg.model.dir + cfg.model.faceProto
         faceModel = cfg.model.dir + cfg.model.faceModel
 
@@ -20,6 +19,7 @@ class GAGModel():
 
         genderProto = cfg.model.dir + cfg.model.genderProto
         genderModel = cfg.model.dir + cfg.model.genderModel
+
         self.MODEL_MEAN_VALUES = cfg.predict.mean
         self.ageList = cfg.predict.ages
         self.genderList = cfg.predict.genders
@@ -50,9 +50,11 @@ class GAGModel():
                 cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
         return frameOpencvDnn, bboxes
 
-    def get_age_gender(self, frame):
+    def get_age_gender(self, frame, returnJson=False):
+        if isinstance(frame, str): frame = cv.imread(frame)
         t = time.time()
         frameFace, bboxes = self.getFaceBox(self.faceNet, frame)
+        data = []
         for bbox in bboxes:
             # print(bbox)
             face = frame[max(0,bbox[1]-self.padding):min(bbox[3]+self.padding,frame.shape[0]-1),max(0,bbox[0]-self.padding):min(bbox[2]+self.padding, frame.shape[1]-1)]
@@ -65,19 +67,9 @@ class GAGModel():
             agePreds = self.ageNet.forward()
             age = self.ageList[int(agePreds[0].argmax())]
 
-            label = "{},{}".format(gender, age)
-            cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
-        return frameFace
-
-
-@hydra.main(config_path="conf", config_name="config")
-def run(cfg: DictConfig) -> None:
-    gag = GAGModel(cfg)
-
-    input1 = cv.imread(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/' + "image.jpg")
-    output = gag.get_age_gender(input1)
-    cv.imshow("!!", output)
-    cv.waitKey()
-
-if __name__ == "__main__":
-    run(testfile=1)
+            if returnJson: 
+                data.append([gender,age])
+            else:
+                label = "{},{}".format(gender, age)
+                cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+        return data if returnJson else frameFace
